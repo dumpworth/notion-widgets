498784fd876e881fdec96de77b18a81bb19baf8c
dumpworth <61677701+dumpworth@users.noreply.github.com>
2025-04-30 02:17:38 -0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function getQueryParam(name) {
-  const params = new URLSearchParams(window.location.search);
-  return parseInt(params.get(name)) || 0;
-}
-
-setProgress(getQueryParam("progress"));
